ff155cae00d16349082f897588fb39599c0d672e
minsoo kim <zlemzlem5656@naver.com>
2021-08-24 02:48:22 +0900

    [추가] sqlite 및 DB 설정

---
+++ lib/database/db.dart
@@ -0,0 +1,94 @@
+import 'dart:io';
+
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+import 'package:todo_practice/database/memo.dart';
+
+final String TableName = 'memos';
+
+class DBHelper {
+  var _db;
+
+  Future<Database> get database async {
+    if (_db != null) return _db;
+
+    _db = await openDatabase(
+      // 데이터베이스 경로를 지정합니다. 참고: `path` 패키지의 `join` 함수를 사용하는 것이
+      // 각 플랫폼 별로 경로가 제대로 생성됐는지 보장할 수 있는 가장 좋은 방법입니다.
+      join(await getDatabasesPath(), 'memos.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE memos(id INTEGER PRIMARY KEY, title TEXT, text TEXT, createTime TEXT, editTime TEXT)",
+        );
+      },
+      // 버전을 설정하세요. onCreate 함수에서 수행되며 데이터베이스 업그레이드와 다운그레이드를
+      // 수행하기 위한 경로를 제공합니다
+      version: 1,
+    );
+    return _db;
+  }
+
+  Future<void> insertMemo(Memo memo) async {
+    final db = await database;
+
+    // Dog를 올바른 테이블에 추가합니다. 동일한 dog가 두번 추가되는 경우를 처리하기 위해
+    // `conflictAlgorithm`을 명시할 수 있습니다.
+    //
+    // 본 예제에서는, 이전 데이터를 갱신하도록 하겠습니다.
+    await db.insert(
+      TableName,
+      memo.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+// dogs 테이블의 모든 dog를 얻는 메서드
+  Future<List<Memo>> memos() async {
+    // 데이터베이스 reference를 얻습니다.
+    final Database db = await database;
+
+    // 모든 Dog를 얻기 위해 테이블에 질의합니다.
+    final List<Map<String, dynamic>> maps = await db.query('memos');
+
+    // List<Map<String, dynamic>를 List<Dog>으로 변환합니다.
+    return List.generate(maps.length, (i) {
+      return Memo(
+        id: maps[i]['id'],
+        title: maps[i]['title'],
+        text: maps[i]['text'],
+        createTime: maps[i]['createTime'],
+        editTime: maps[i]['editTime'],
+      );
+    });
+  }
+
+  Future<void> updateMemo(Memo memo) async {
+    // 데이터베이스 reference를 얻습니다.
+    final db = await database;
+
+    // 주어진 Dog를 수정합니다.
+    await db.update(
+      TableName,
+      memo.toMap(),
+      // Dog의 id가 일치하는 지 확인합니다.
+      where: "id = ?",
+      // Dog의 id를 whereArg로 넘겨 SQL injection을 방지합니다.
+      whereArgs: [memo.id],
+    );
+  }
+
+  Future<void> deleteMemo(int id) async {
+    // 데이터베이스 reference를 얻습니다.
+    final db = await database;
+
+    // 데이터베이스에서 Dog를 삭제합니다.
+    await db.delete(
+      TableName,
+      // 특정 dog를 제거하기 위해 `where` 절을 사용하세요
+      where: "id = ?",
+      // Dog의 id를 where의 인자로 넘겨 SQL injection을 방지합니다.
+      whereArgs: [id],
+    );
+  }
+}
--- lib/screens/edit.dart
+++ lib/screens/edit.dart
@@ -1,17 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:todo_practice/database/db.dart';
+import 'package:todo_practice/database/memo.dart';
 
 class EditPage extends StatelessWidget {
-  const EditPage({Key? key}) : super(key: key);
+  String title = '';
+  String text = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('hi'),
-      ),
-      body: Center(
-        child: Text('hi'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('hi'),
+          actions: <Widget>[
+            IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+            IconButton(onPressed: saveDB, icon: Icon(Icons.save))
+          ],
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextField(
+                // obscureText: true,
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                decoration: InputDecoration(hintText: '제목을 적어주세요'),
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                onChanged: (String title) {
+                  this.title = title;
+                },
+              ),
+              Padding(padding: EdgeInsets.only(top: 20)),
+              TextField(
+                // obscureText: true,
+                decoration: InputDecoration(hintText: '메모의 내용을 적어주세요'),
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                onChanged: (String text) {
+                  this.text = text;
+                },
+              ),
+            ],
+          ),
+        ));
+  }
+
+  Future<void> saveDB() async {
+    DBHelper sd = DBHelper();
+
+    var fido = Memo(
+        id: 1,
+        title: this.title,
+        text: this.text,
+        createTime: DateTime.now().toString(),
+        editTime: DateTime.now().toString());
+
+    await sd.insertMemo(fido);
+
+    print(await sd.memos());
   }
 }
